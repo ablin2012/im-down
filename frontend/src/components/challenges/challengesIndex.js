8387baf7f2b24cd7db5aec1f788ca4237e639707
Quang Tran <quang.tran.769869@gmail.com>
2022-07-26 16:12:58 -0700

    built initial search functionality

--- frontend/src/components/challenges/challengesIndex.js
+++ frontend/src/components/challenges/challengesIndex.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import './challenge.css'
+import NavBarContainer from '../nav/navBarContainer';
 
 import ChallengesIndexItem from './challengesIndexItem';
 
@@ -8,8 +9,11 @@ class ChallengesIndex extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            challenges: []
+            challenges: [],
+            filter: ""
         }
+        this.handleCallback = this.handleCallback.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
     }
 
     componentWillMount() {
@@ -18,24 +22,45 @@ class ChallengesIndex extends React.Component {
 
     componentWillReceiveProps(newState) {
         this.setState({ challenges: newState.challenges });
+        // this.handleCallback()
+    }
+
+    handleCallback = (navSearchData) => this.setState({'filter': navSearchData})
+    
+    handleSearch() {
+        if(this.state.filter === "") {
+            return this.state.challenges
+        } else {
+            return this.state.challenges.filter(card => card.title.match(new RegExp(this.state.filter, "i")) || card.description.match(new RegExp(this.state.filter, "i")))
+        }
     }
 
     render() {
+        
         if (this.state.challenges.length === 0) {
-            return (<div>There are no Challenges</div>)
+            return (<div className='body-wrap'>
+                    <div>There are no Challenges</div>
+                </div>
+                )
         } else {
             return (
-                <div className='challenge-cards-container'>
-                    <h2>All Challenges</h2>
-                    
-                    {this.state.challenges.map(challenge => (
-                        <ChallengesIndexItem 
-                            key={challenge._id} 
-                            title={challenge.title}
-                            description={challenge.description}
-                            category={challenge.category} />
-                    ))}
-                </div>
+                <>
+                    <header>
+                        <NavBarContainer parentCallback={this.handleCallback}/>
+                    </header>
+                    <div className='body-wrap'>
+                        <div className='challenge-cards-container'>
+                            <h2>All Challenges</h2>
+                            {this.handleSearch().map(challenge => (
+                                <ChallengesIndexItem 
+                                    key={challenge._id} 
+                                    title={challenge.title}
+                                    description={challenge.description}
+                                    category={challenge.category} />
+                            ))}
+                        </div>
+                    </div>
+                </>
             );
         }
     }
