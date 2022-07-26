d0eca927e7ba55437fe8a626be29046117bfaf91
ablin2012 <adamlin.2009@gmail.com>
2022-07-25 20:15:28 -0700

    Challenges can be uploaded with photos

--- routes/api/challenges.js
+++ routes/api/challenges.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');              // multer will be used to handle the form data.
+const Aws = require('aws-sdk');                // aws-sdk library will used to upload image to s3 bucket. 
 const mongoose = require('mongoose');
 const passport = require('passport');
+require("dotenv/config")
 
 const Challenge = require('../../models/Challenge');
 const Participation = require('../../models/Participation');
 const validateChallengeInput = require('../../validation/challenges');
 
+// AWS Stuff
+const storage = multer.memoryStorage({
+  destination: function (req, file, cb) {
+      cb(null, '')
+  }
+});
+
+const filefilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg') {
+      cb(null, true)
+  } else {
+      cb(null, false)
+  }
+}
+
+const upload = multer({ storage: storage, fileFilter: filefilter });
+
+const s3 = new Aws.S3({
+  accessKeyId:process.env.AWS_ACCESS_KEY_ID,              // accessKeyId that is stored in .env file
+  secretAccessKey:process.env.AWS_ACCESS_KEY_SECRET       // secretAccessKey is also store in .env file
+})
+
+
+
+// Routes
 router.get('/', (req, res) => {
     Challenge.find()
         .sort({ date: -1 })
@@ -50,42 +78,58 @@ router.delete('/:id',
         })
 });
 
-router.post('/',
+router.post('/', upload.single('challengeimage'),
     passport.authenticate('jwt', { session: false }),
     (req, res) => {
+      console.log(req);
       const { errors, isValid } = validateChallengeInput(req.body);
   
       if (!isValid) {
         return res.status(400).json(errors);
       }
   
-      const newChallenge = new Challenge({
-        title: req.body.title,
-        description: req.body.description,
-        creator: req.user.id,
-        startDate: req.body.startDate,
-        endDate: req.body.endDate,
-        category: req.body.category
-      });
-  
-      newChallenge.save().then(challenge => {
-        const newPost = new Post({
-          text: `I just created a challenge!`,
-          type: `create`,
-          user: req.user.id,
-          challenge: challenge.id
-        })
+      const params = {
+        Bucket:process.env.AWS_BUCKET_NAME,      // bucket that we made earlier
+        Key:req.file.originalname,               // Name of the image
+        Body:req.file.buffer,                    // Body which will contain the image in buffer format
+        ACL:"public-read-write",                 // defining the permissions to get the public link
+        ContentType:"image/jpeg"                 // Necessary to define the image content-type to view the photo in the browser with the link
+      };
 
-        newPost.save().then(post => {
-          const newParticipation = new Participation({
-            challenge: post.challenge,
-            participant: req.user.id
+      s3.upload(params,(error,data)=>{
+        if(error){
+            res.status(500).send({"err":error})  // if we get any error while uploading error message will be returned.
+        }
+
+
+        const newChallenge = new Challenge({
+          title: req.body.title,
+          description: req.body.description,
+          creator: req.user.id,
+          startDate: req.body.startDate,
+          endDate: req.body.endDate,
+          category: req.body.category,
+          challengeImage: data.Location
+        });
+    
+        newChallenge.save().then(challenge => {
+          const newPost = new Post({
+            text: `I just created a challenge!`,
+            type: `create`,
+            user: req.user.id,
+            challenge: challenge.id
           })
-  
-          newParticipation.save().then(participation => res.json(challenge))
-        })
-      });
 
+          newPost.save().then(post => {
+            const newParticipation = new Participation({
+              challenge: post.challenge,
+              participant: req.user.id
+            })
+    
+            newParticipation.save().then(participation => res.json(challenge))
+          })
+        });
+      });
     }
 );
 
