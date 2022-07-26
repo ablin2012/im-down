2948ac7254ebf291937dcb48e1ffff263125cfe6
ablin2012 <adamlin.2009@gmail.com>
2022-07-26 16:54:20 -0700

    Create post Index

---
+++ frontend/src/reducers/postsReducer.js
@@ -0,0 +1,24 @@
+import {RECEIVE_CHALLENGE_POSTS, RECEIVE_NEW_POST, RECEIVE_POSTS, RECEIVE_USER_POSTS} from '../actions/postActions';
+
+const PostsReducer = (state = { all: {}, user: {}, challenge: {}, new: undefined}, action) => {
+    Object.freeze(state);
+    let newState = Object.assign({}, state);
+    switch (action.type) {
+        case RECEIVE_POSTS:
+            newState.all = action.posts.data;
+            return newState;
+        case RECEIVE_USER_POSTS:
+            newState.user = action.posts.data;
+            return newState;
+        case RECEIVE_CHALLENGE_POSTS:
+            newState.challenge = action.posts.data;
+            return newState;
+        case RECEIVE_NEW_POST:
+            newState.new = action.post.data;
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default PostsReducer;
--- frontend/src/util/postsApiUtil.js
+++ frontend/src/util/postsApiUtil.js
@@ -4,3 +4,14 @@ export const getPosts = () => {
     return axios.get('/api/posts')
 }
 
+export const getUserPosts = (id) => {
+    return axios.get(`/api/posts/user/${id}`)
+}
+
+export const getChallengePosts = (id) => {
+    return axios.get(`/api/posts/challenge/${id}`)
+}
+
+export const createPost = ({post, challengeId}) => {
+    return axios.post(`/api/posts/challenge/${challengeId}`, post)
+}
