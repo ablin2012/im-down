452cee03a59727333d6c4e173f7ce8379b339cd1
Lucy Luo <lucyluo08@gmail.com>
2022-07-27 10:46:40 -0700

    add container

---
+++ frontend/src/components/profile/userShowContainer.js
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom'
+import { fetchUserChallenges } from '../../actions/challengeActions';
+import { fetchUser, fetchUserAchievements } from '../../actions/userActions';
+import UserShow from './userShow';
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        challenges: Object.values(state.challenges.user),
+        achivements: Object.values(state.achivements),
+        currentUser: state.session.user,
+        user: state.users[ownProps.match.params.user_id],
+    };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        fetchUserChallenges: id => dispatch(fetchUserChallenges(id)),
+        fetchUserAchievements: id => dispatch(fetchUserAchievements(id)),
+        fetchUser: id => dispatch(fetchUser(id)),
+    };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(UserShow));
