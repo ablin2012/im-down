4b29d81627fdd43876b02b3cde76726f6b82b7de
ablin2012 <adamlin.2009@gmail.com>
2022-07-31 20:50:34 -0700

    heroku stuff

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ mongoose
 .then(() => console.log("Connected to MongoDB successfully"))
 .catch(err => console.log(err));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
@@ -33,10 +40,3 @@ app.use("/api/participations", participations);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-    app.get('/', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    })
-}
