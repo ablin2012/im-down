b085a4a128198bfdb3de301fcc8b671ae39b6dee
ablin2012 <adamlin.2009@gmail.com>
2022-07-31 20:42:17 -0700

    heroku stuff

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const db = require('./config/keys_dev').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require("./routes/api/users");
 const challenges = require("./routes/api/challenges");
@@ -32,3 +33,10 @@ app.use("/api/participations", participations);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
